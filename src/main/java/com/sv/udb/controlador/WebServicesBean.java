d1cfd0d919096d763299dc0e689993b12cc1315c
Mauricio <mauricio_gonzalez@ricaldone.edu.sv>
2016-11-02 17:41:42 -0600

    Agregado WebServices Client Lists

--- src/main/java/com/sv/udb/controlador/WebServicesBean.java
+++ src/main/java/com/sv/udb/controlador/WebServicesBean.java
@@ -6,16 +6,21 @@
 package com.sv.udb.controlador;
 
 import com.sv.udb.utils.AlumnosPojo;
+import com.sv.udb.utils.pojos.DatosAlumnos;
+import com.sv.udb.utils.pojos.WSconsAlumByDoce;
 import java.io.Serializable;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status.Family;
+import org.apache.commons.codec.Charsets;
 import org.primefaces.context.RequestContext;
 
 /**
@@ -29,6 +34,7 @@ public class WebServicesBean implements Serializable{
     private static final long serialVersionUID = 1L;
     private String nombUsua;
     private String filt; //Filotro de búsqueda
+    private WSconsAlumByDoce objeWebServ;
 
     public String getNombUsua() {
         return nombUsua;
@@ -45,6 +51,14 @@ public class WebServicesBean implements Serializable{
     public void setFilt(String filt) {
         this.filt = filt;
     }
+
+    public WSconsAlumByDoce getObjeWebServ() {
+        return objeWebServ;
+    }
+
+    public void setObjeWebServ(WSconsAlumByDoce objeWebServ) {
+        this.objeWebServ = objeWebServ;
+    }
     
     /**
      * Creates a new instance of WebServicesBean
@@ -60,20 +74,26 @@ public class WebServicesBean implements Serializable{
     
     public void consWebServ()
     {
+        FacesContext facsCtxt = FacesContext.getCurrentInstance();
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página
         Client client = ClientBuilder.newClient();
-        String url = String.format("http://localhost:8080/WebService/poo/MiServicio/%s", this.filt);
+        String url = facsCtxt.getExternalContext().getInitParameter("webservices.URL"); //Esta en el web.xml
+        url = String.format("%s/%s/%s", url, "consAlumByDoce", this.filt);
         WebTarget resource = client.target(url);
         Builder request = resource.request();
-        request.accept(MediaType.APPLICATION_JSON);
+        request.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_TYPE.withCharset("utf-8"));
         Response response = request.get();
         if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL)
         {
-            AlumnosPojo resp = response.readEntity(AlumnosPojo.class); //La respuesta de captura en un pojo que esta en el paquete utils
-            this.nombUsua = resp.getNomb();
+            this.objeWebServ = response.readEntity(WSconsAlumByDoce.class); //La respuesta de captura en un pojo que esta en el paquete utils
+//            for(DatosAlumnos temp : this.objeWebServ.getResu())
+//            {
+//                System.err.println(String.format("Carnet: %s Nombre: %s", temp.getCarn(), temp.getNomb()));
+//            }
         }
         else
         {
-            this.nombUsua = "No hay nada";
+            ctx.execute("setMessage('MESS_ERRO', 'Atención', 'Error al procesar la consulta')");
         }
     }
     
